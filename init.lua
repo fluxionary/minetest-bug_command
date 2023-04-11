8dd382f06dd13fae84295ee87704b89ba0c64946
flux <25628292+fluxionary@users.noreply.github.com>
2023-04-11 14:37:29 -0700

    fix bug

--- init.lua
+++ init.lua
@@ -4,6 +4,12 @@ if not http then
 	error("bug_command requires access to the http api; please add it to secure.http_mods in minetest.conf")
 end
 
+futil.check_version({ year = 2023, month = 4, day = 11 })
+
+bug_command = fmod.create(nil, {
+	http = http,
+})
+
 if not bug_command.settings.github_token then
 	error("github token required; set bug_command:github_token in minetest.conf")
 end
@@ -11,12 +17,6 @@ end
 if not bug_command.settings.github_repo then
 	error("github repo required; set bug_command:github_repo in minetest.conf")
 end
-
-futil.check_version({ year = 2023, month = 4, day = 11 })
-
-bug_command = fmod.create(nil, {
-	http = http,
-})
 
 bug_command.dofile("bug_command")
 bug_command.dofile("bugs_command")
